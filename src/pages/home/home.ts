8af9902af8439cfb022a8654aef7fdc86282dd12
lordneru <lordneru@gmail.com>
2018-03-17 19:37:49 +0100

    starting maps basis

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
+
+declare var google: any;
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,34 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  @ViewChild('map') mapRef:ElementRef;
+
   constructor(public navCtrl: NavController) {
 
+  }
+
+  ionViewDidLoad() {
+    this.initMap();
+  }
+
+  initMap(){
+    const location = new google.maps.LatLng(36.686788,-6.126611);
+
+    const option = {
+      center: location,
+      zoom: 13
+    };
+
+    let map = new google.maps.Map(this.mapRef.nativeElement, option);
+
+    this.addMarker(location, map);
+  }
+
+  addMarker(position, map){
+    return new google.maps.Marker({
+      position,
+      map
+    });
   }
 
 }
