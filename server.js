9b22cb13c00005c0e80a1bdf33d8eb9761421307
lordneru <lordneru@gmail.com>
2018-03-19 22:06:02 +0100

    heroku deployment changes

---
+++ server.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('www'));
+
+app.set('port', process.env.PORT || 500);
+app.listen(app.get('port'), function(){
+    console.log('Express ionic gitserver listing on port ' + app.get('port'));
+})
